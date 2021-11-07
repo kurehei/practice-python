61df54b666fa8984ec226de0f08591fd246fe1bb
榑林晃平 <kureykou321@gmail.com>
2021-11-08 00:18:07 +0900

    文字の操作

---
+++ basic/base7.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 文字列
+
+fruit = "apple"
+print(fruit)
+print(type(fruit))
+
+# 10回出力
+print(fruit * 10)
+
+fruits = """apple
+banana
+orange
+"""
+print(fruits)
+
+fruit2 = "banana"
+byte_fruit = fruit2.encode('utf-8')
+print(byte_fruit)
+print(type(byte_fruit))
+
+# encode decode byte[]
